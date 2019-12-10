fd410408db386bb1efebbc02ba69c42598aa47a0
Anthony Styzhin <astyzhin@gmail.com>
2019-12-10 08:21:50 +0300

    Added: App dagger application Updated: layouts and added some drawable resources

--- app/src/main/java/com/akmisoftware/noteit/App.kt
+++ app/src/main/java/com/akmisoftware/noteit/App.kt
@@ -1,5 +1,14 @@
 package com.akmisoftware.noteit
 
-class App {
+import com.akmisoftware.noteit.di.DaggerCoreComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
 
+class App : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerCoreComponent
+            .builder()
+            .application(this)
+            .build()
+    }
 }
